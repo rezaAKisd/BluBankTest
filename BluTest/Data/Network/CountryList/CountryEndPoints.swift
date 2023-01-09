ee9c480e1c4cf4d29774fbba1294da8f4730d09d
reza <rezaakbariiosdeveloper@gmail.com>
2023-01-09 09:55:09 +0330

    [EDIT] remove moya CountryEndpoint

--- BluTest/Data/Network/CountryList/CountryEndPoints.swift
+++ BluTest/Data/Network/CountryList/CountryEndPoints.swift
@@ -6,55 +6,6 @@
 //
 
 import Foundation
-import Moya
-
-// enum CountryEndPoints {
-//    case getCountryList
-// }
-//
-// extension CountryEndPoints: TargetType {
-//    public var baseURL: URL {
-//        switch self {
-//        case .getCountryList:
-//            return URL(string: AppConfiguration.apiBaseURL)!
-//        }
-//    }
-//
-//    public var headers: [String: String]? {
-//        nil
-//    }
-//
-//    public var method: Moya.Method {
-//        return .get
-//    }
-//
-//    public var path: String {
-//        switch self {
-//        case .getCountryList:
-//            return "v3.1/all"
-//        }
-//    }
-//
-//    public var sampleData: Data {
-//        return Data()
-//    }
-//
-//    public var task: Task {
-//        switch self {
-//        case .getCountryList:
-//            return .requestPlain
-//        }
-//    }
-//
-//    public var validationType: ValidationType {
-//        return .successAndRedirectCodes
-//    }
-//
-//    var authorizationType: AuthorizationType? {
-//        .none
-//    }
-// }
-//
 
 enum CountryEndPoints {
     case getCountryList
